aca260aaf644651a7841e4ab215f90cf0822a1d8
HvRs <hvrsttk@gmail.com>
2022-05-30 16:48:56 +0700

    comm appl

---
+++ PostgreSqlDotnetCore/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PostgreSqlDotnetCore.Models;
+using PostgreSqlDotnetCore.Views;
+
+namespace PostgreSqlDotnetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult AddApplication()
+        {
+            return View();
+        }
+        public IActionResult ListApplication()
+        {
+            return View();
+        }
+
+        public IActionResult Stats()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult AddApplication(string name, string email)
+        {
+            Random random = new Random();
+             string Ucode = String.Empty;
+            int r;
+            char[] ch4r = "abcdefghijklmnopqrstuvwxyzABCDEFGHUJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()!№;:?_-=+".ToCharArray();
+            for (int i = 0; i < 64; i++)
+            {
+                r = random.Next(ch4r.Length);
+                Ucode += ch4r[r];
+            }
+            DB database = new DB();
+            database.addApp(name, Ucode, email);
+            return View(database);
+        }
+    }
+}
---
+++ PostgreSqlDotnetCore/Views/DB.cs
@@ -0,0 +1,75 @@
+﻿namespace PostgreSqlDotnetCore.Views
+{
+    using Npgsql;
+    using System;
+    using System.Data;
+    using System.Data.Common;
+
+    public class DB
+    {
+        public string connectionString = "Host=localhost;Username=postgres;Password='1234567890';Database=olimpiada";
+            
+        public void amountApp(string email, out int amount)
+        {
+            amount = 0;
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+            npgSqlConnection.Open();
+            NpgsqlCommand npgSqlCommand = new NpgsqlCommand($@"SELECT name FROM  public.""AspNetUserApplication"" WHERE email = '{email}';", npgSqlConnection);
+            NpgsqlDataReader npgSqlDataReader = npgSqlCommand.ExecuteReader();
+            if (npgSqlDataReader.HasRows)
+                foreach (DbDataRecord dbDataRecord in npgSqlDataReader)
+                    amount++;
+            npgSqlConnection.Close();
+        }
+
+        public void addApp(string name, string ucode, string email)
+        {
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+            npgSqlConnection.Open();
+             NpgsqlCommand npgSqlCommand = new NpgsqlCommand($@"INSERT INTO public.""AspNetUserApplication""(email, name, ucode, date, view, edit) VALUES ('{email}', '{name}', '{ucode}', '{DateTime.Now:g}', '0', '1')", npgSqlConnection);
+            npgSqlCommand.ExecuteNonQuery();
+            npgSqlConnection.Close();
+        }
+
+        
+        public void statsApp(string email, out int view, out int edit)
+        {
+            view = 1; edit = 1;
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+            npgSqlConnection.Open();
+            NpgsqlCommand npgSqlCommand = new NpgsqlCommand($@"SELECT view, edit FROM public.""AspNetUserApplication"" WHERE email = '{email}';", npgSqlConnection);
+            NpgsqlDataReader npgSqlDataReader = npgSqlCommand.ExecuteReader();
+            if (npgSqlDataReader.HasRows)
+                foreach (DbDataRecord dbDataRecord in npgSqlDataReader)
+                {
+                    if(view < int.Parse(dbDataRecord["view"].ToString()) && edit < int.Parse(dbDataRecord["edit"].ToString()))
+                         view = int.Parse(dbDataRecord["view"].ToString()); edit += int.Parse(dbDataRecord["edit"].ToString());
+       
+                }
+
+            npgSqlConnection.Close();
+        }
+
+        public void view(string email)
+        {
+            int view = 0;
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+            npgSqlConnection.Open();
+            NpgsqlCommand npgSqlCommand = new NpgsqlCommand($@"SELECT view FROM public.""AspNetUserApplication"" WHERE email = '{email}';", npgSqlConnection);
+            NpgsqlDataReader npgSqlDataReader = npgSqlCommand.ExecuteReader();
+            if (npgSqlDataReader.HasRows)
+                foreach (DbDataRecord dbDataRecord in npgSqlDataReader)
+                        view = int.Parse(dbDataRecord["view"].ToString());
+            addview(email, view);
+        }
+
+        public void addview(string email, int view)
+        {
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+            npgSqlConnection.Open();
+            NpgsqlCommand npgSqlCommand2 = new NpgsqlCommand($@"UPDATE ""AspNetUserApplication"" SET view = {view+1} WHERE email = '{email}';", npgSqlConnection);
+            npgSqlCommand2.ExecuteNonQuery();
+            npgSqlConnection.Close();
+        }
+    }
+}
